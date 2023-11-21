61000d917b103cd47da453520eea79fdf42698da
Matheus M <matheus150101miranda@gmail.com>
2023-11-21 09:57:06 -0300

    update branch

--- src/contexts/GuestContext/index.tsx
+++ src/contexts/GuestContext/index.tsx
@@ -9,35 +9,35 @@ import { TGuestLoginData } from "../../validators/guestValidators";
 export const GuestContext = createContext<IGuestContext>({} as IGuestContext);
 
 export const GuestProvider = ({ children }: IChildrenProps) => {
-    const [guest, setGuest] = useState<iGuest | null>(null)
-    const navigate = useNavigate()
+  const [guest, setGuest] = useState<iGuest | null>(null);
+  const navigate = useNavigate();
 
-    const guestLogin = async (formData: TGuestLoginData) => {
-      console.log("olá")  
-      try {
-          const response = await api.post("/guest/login/", formData);
-          // console.log(response.data)
-          setGuest(response.data.user);
-          localStorage.setItem("@DataHotel:TOKEN", response.data.accessToken);
-          localStorage.setItem("@DataHotel:ID", response.data.user.guest.id);
-          toast.success("Login efetuado com sucesso");
-          navigate("/");
-        } catch (error) {
-          console.log(error);
-          toast.error("Tente novamente");
-        }
-      };
-    
-      return (
-        <GuestContext.Provider
-          value={{
-            navigate,
-            guestLogin
-          }}
-        >
-          {children}
-        </GuestContext.Provider>
-      );
-}
+  const guestLogin = async (formData: TGuestLoginData) => {
+    console.log("olá");
+    try {
+      const response = await api.post("/guest/login/", formData);
+      // console.log(response.data)
+      setGuest(response.data.user);
+      localStorage.setItem("@DataHotel:TOKEN", response.data.accessToken);
+      localStorage.setItem("@DataHotel:ID", response.data.user.guest.id);
+      toast.success("Login efetuado com sucesso");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("Tente novamente");
+    }
+  };
 
-export const useGuest= () => useContext(GuestContext);
+  return (
+    <GuestContext.Provider
+      value={{
+        navigate,
+        guestLogin,
+      }}
+    >
+      {children}
+    </GuestContext.Provider>
+  );
+};
+
+export const useGuest = () => useContext(GuestContext);
